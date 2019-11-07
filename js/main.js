600cd387dc264a3213b95648e6f009c84ef2b211
ranveerverma709@gmail.com <ranveerverma709@gmail.com>
2019-11-07 23:30:07 +0530

    Added Visual Effects

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
-$('#navbar a, .read').on('click', function(event) {
-  if(this.hash !== ''){
-  event.preventDefault();
+$("#navbar a, .read").on("click", function(event) {
+  if (this.hash !== "") {
+    event.preventDefault();
 
-  const hash=this.hash;
-  $('html, body').animate(
-    {
-      scrollTop: $(hash).offset().top -100,
-    },
-    800,
-   );
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
   }
 });
+function scrollApppear() {
+  var target = document.querySelectorAll(".visual");
+  console.log(target);
+  target.forEach(target => {
+    var targetPosition = target.getBoundingClientRect().top;
+    var screenPosition = window.innerHeight / 1.5;
+    console.log(targetPosition);
+    console.log(screenPosition);
+    if (targetPosition < screenPosition) {
+      target.classList.add("bg-active");
+    }
+  });
+}
+window.addEventListener("scroll", scrollApppear);
